eab6fa3379be0fcd1d4dcfc50b5d64f6c658982c
philipyexushen <923822778@qq.com>
2019-06-23 23:12:35 +0800

    命名问题

--- ImageRestoration.py
+++ ImageRestoration.py
@@ -5,43 +5,43 @@ from numba import jit
 import time
 
 
-@jit
-def check_point_pulse(z_value: int, z_max: int, z_min: int, z_med: np.float) -> int:
-    if z_value - z_max < 0 < z_value - z_min:
-        return z_value
-    else:
-        return int(z_med)
-
-@jit
-def find_not_pulse(img: np.ndarray, point: tuple, max_feature_size: tuple = (20, 20))->int:
-    s_size:tuple = (3, 3)
-    width, height = img.shape[:2]
-
-    ret_value = img[point]
-    while s_size[0] <= max_feature_size[0] and  s_size[1] <= max_feature_size[1]:
-        left = int(max(point[0] - s_size[0] // 2, 0))
-        right = int(min(point[0] + s_size[0] // 2 + 1, width))
-        bottom = int(max(point[1] - s_size[1] // 2, 0))
-        top = int(min(point[1] + s_size[1] // 2 + 1, height))
-
-        s:np.ndarray = img[left: right, bottom: top]
-        z_max, z_min, z_med = np.max(s), np.min(s), np.median(s)
-
-        a1 = z_med - z_min
-        a2 = z_med - z_max
-
-        if a2 < 0 < a1:
-            ret_value:int = check_point_pulse(img[point], z_max, z_min, np.float64(z_med))
-            break
-        else:
-            s_size = (s_size[0] + 1, s_size[1] + 1)
-
-    return ret_value
-
-
 class AdaptiveMedianBlur:
     @classmethod
     def remove_noise(cls, img: np.ndarray)->np.ndarray:
+        @jit
+        # 注意jit函数不能写成classMethod或者是staticMethod，会让numba无法优化的
+        def check_point_pulse(z_value: int, z_max: int, z_min: int, z_med: np.float) -> int:
+            if z_value - z_max < 0 < z_value - z_min:
+                return z_value
+            else:
+                return int(z_med)
+
+        @jit
+        def find_not_pulse(img: np.ndarray, point: tuple, max_feature_size: tuple = (20, 20)) -> int:
+            s_size: tuple = (3, 3)
+            _width, _height = img.shape[:2]
+
+            ret_value = img[point]
+            while s_size[0] <= max_feature_size[0] and s_size[1] <= max_feature_size[1]:
+                left = int(max(point[0] - s_size[0] // 2, 0))
+                right = int(min(point[0] + s_size[0] // 2 + 1, _width))
+                bottom = int(max(point[1] - s_size[1] // 2, 0))
+                top = int(min(point[1] + s_size[1] // 2 + 1, _height))
+
+                s: np.ndarray = img[left: right, bottom: top]
+                z_max, z_min, z_med = np.max(s), np.min(s), np.median(s)
+
+                a1 = z_med - z_min
+                a2 = z_med - z_max
+
+                if a2 < 0 < a1:
+                    ret_value: int = check_point_pulse(img[point], z_max, z_min, np.float64(z_med))
+                    break
+                else:
+                    s_size = (s_size[0] + 1, s_size[1] + 1)
+
+            return ret_value
+
         width, height = img.shape[:2]
         img_ret = img.copy()
 
